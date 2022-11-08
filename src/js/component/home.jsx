1fde8bd4a5a17620489b6207487760e76650a2c9
ChuchoAraujo <jeseazuara@gmail.com>
2022-11-08 15:02:24 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,104 @@
-import React from "react";
+import { element } from "prop-types";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+
+	const [array, setArray] = useState([])
+	const [input, setInput] =useState("")
+
+	const changeInputvalue = (e) => {
+		setInput(e.target.value)
+	}
+
+    // FUNCION GET //
+
+
+    const getAllelements = () => {
+		var requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/chuchoazuara", requestOptions)
+			.then(response => response.json())
+			.then(result => setArray(result))
+			.catch(error => console.log('error', error));
+	}
+
+	// FUNCION POST //
+
+	const createElement = () => {
+		var myHeaders = new Headers();
+         myHeaders.append("Content-Type", "application/json");
+
+
+		 let agregar = array.concat( {
+			"label": input,
+			"done": false
+		  })
+
+         var requestOptions = {
+           method: 'PUT',
+           headers: myHeaders,
+           body: JSON.stringify(agregar),
+           redirect: 'follow'
+         };
+
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/chuchoazuara", requestOptions)
+          .then(response => response.json())
+          .then(result => getAllelements())
+          .catch(error => console.log('error', error));
+	        }
+    
+	// FUNCION DELETE //
+
+	const deleteElement = (element) => {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+
+		let eliminar = array.filter(task => task != element)
+		console.log(eliminar)
+
+		var requestOptions = {
+		  method: 'PUT',
+		  headers: myHeaders,
+		  body: JSON.stringify(eliminar),
+		  redirect: 'follow'
+		};
+
+	   fetch("https://assets.breatheco.de/apis/fake/todos/user/chuchoazuara", requestOptions)
+		 .then(response => response.json())
+		 .then(result => getAllelements())
+		 .catch(error => console.log('error', error));
+		   }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Lista Tareas</h1>
+			{
+				array.map(((element, index) => 
+					<div className="row">
+						<div className="col-4"></div>
+						<div className="col-4 contenedorTarea">
+						      <p  key={index} className="tareaTexto">
+								{element.label}
+					          <button className="botonEliminar ms-5" onClick={() => deleteElement(element)}>Eliminar</button>
+					          </p>
+						</div>
+
+						 <div className="col-4"></div>
+					</div>
+
+				))
+			}
+
+			<p><input className="input" onChange={changeInputvalue} type="text" /></p>
+			<button  onClick={getAllelements}>Obtener lista</button>
+			<button className="ms-3" onClick={createElement}>Create Element</button>
 		</div>
-	);
-};
+	)
+}
 
 export default Home;
